b567e48871978b16e7045d9a3dbb88ee382eca99
Nikola Lukovic <nikola.lukovic@live.com>
2023-02-20 18:22:23 +0100

    Initial commit

---
+++ Primal/PrimalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimalApp.swift
+//  Primal
+//
+//  Created by Nikola Lukovic on 20.2.23..
+//
+
+import SwiftUI
+
+@main
+struct PrimalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
