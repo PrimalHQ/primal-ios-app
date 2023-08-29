62a16ebd080cefe6bf63ddc48d3fcf6881f09e22
Nikola Lukovic <nikola.lukovic@live.com>
2023-08-29 13:09:01 +0200

    Implemented encrypt/decrypt of content for DMs
    Base64 supported only

--- PrimalTests/PrimalTests.swift
+++ PrimalTests/PrimalTests.swift
@@ -9,5 +9,17 @@ import XCTest
 @testable import Primal
 
 final class PrimalTests: XCTestCase {
-    
+    func testEncryptDecryptDirectMessages() {
+        let keypair = NostrKeypair.generate()
+        let message = "test"
+        
+        guard
+            let privkey = keypair?.hexVariant.privkey,
+            let pubkey = keypair?.hexVariant.pubkey,
+            let encrypted = encryptDirectMessage(message, privkey: privkey, pubkey: pubkey),
+            let decrypted = decryptDirectMessage(encrypted, privkey: privkey, pubkey: pubkey)
+        else { return }
+        
+        XCTAssertEqual(message, decrypted)
+    }
 }
