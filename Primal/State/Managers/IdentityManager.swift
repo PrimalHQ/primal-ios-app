9fe86f0ae93df560bac5da90a72caa5431861b4e
Nikola Lukovic <nikola.lukovic@live.com>
2023-09-29 00:09:40 +0200

    Fixed not presenting the alert with a controller instance when deleted account has been detected

--- Primal/State/Managers/IdentityManager.swift
+++ Primal/State/Managers/IdentityManager.swift
@@ -399,5 +399,8 @@ final class IdentityManager {
             RootViewController.instance.reset()
             UserDefaults.standard.nwc = nil
         })
+        DispatchQueue.main.async {
+            RootViewController.instance.present(alert, animated: true)
+        }
     }
 }
