f82513709002ef44224b6d9dd096b955170d6300
Pavle Stevanović <pavlekzn@gmail.com>
2025-10-23 17:22:17 +0200

    new nwc

--- Primal/State/Managers/WalletManager/NWCWalletManager.swift
+++ Primal/State/Managers/WalletManager/NWCWalletManager.swift
@@ -77,28 +77,63 @@ class NWCWalletManager {
         
         let data = NostrWalletConnect(lightningAddress: nil, relays: [relay], pubkey: serverPubkey, keypair: .init(privateKey: secret, pubkey: secretPubkey))
         
-//        let regConnection = PrimalApiClientFactory.shared.create(serverType: .caching)
-//        let walletConnection = PrimalApiClientFactory.shared.create(serverType: .wallet)
-//        
-//        let repo = IosRepositoryFactory.shared.createProfileRepository(cachingPrimalApiClient: regConnection, primalPublisher: self)
-//        let eventRepo = IosRepositoryFactory.shared.createEventRepository(cachingPrimalApiClient: regConnection)
-//        
-//        // WalletRepo wallet info by id (balance, transactions, etc.)
-//        let walletRepo = IosRepositoryFactory_.shared.createWalletRepository(
-//            primalWalletApiClient: walletConnection,
-//            nostrEventSignatureHandler: self,
-//            profileRepository: repo,
-//            eventRepository: eventRepo
-//        )
-//        
-//        // WalletAccountRepo
-//        let walletAccountRepo = IosRepositoryFactory_.shared.createWalletAccountRepository(primalWalletApiClient: walletConnection, nostrEventSignatureHandler: self)
-        
         client = NwcClientFactory.shared.createNwcApiClient(nwcData: data)
         zapper = NwcClientFactory.shared.createNwcNostrZapper(nwcData: data)
         print("NWC: \(url)")
         
         refreshBalance()
+        
+        let regConnection = PrimalApiClientFactory.shared.create(serverType: .caching)
+        let walletConnection = PrimalApiClientFactory.shared.create(serverType: .wallet)
+        
+        let repo = IosRepositoryFactory.shared.createProfileRepository(cachingPrimalApiClient: regConnection, primalPublisher: self)
+        let eventRepo = IosRepositoryFactory.shared.createEventRepository(cachingPrimalApiClient: regConnection)
+        
+        // WalletRepo wallet info by id (balance, transactions, etc.)
+        let walletRepo = IosRepositoryFactory_.shared.createWalletRepository(
+            primalWalletApiClient: walletConnection,
+            nostrEventSignatureHandler: self,
+            profileRepository: repo,
+            eventRepository: eventRepo
+        )
+        
+        let pubkey = IdentityManager.instance.userHexPubkey
+        
+        
+        
+        
+        // WalletAccountRepo
+        let walletAccountRepo = IosRepositoryFactory_.shared.createWalletAccountRepository(primalWalletApiClient: walletConnection, nostrEventSignatureHandler: self)
+        
+        // Za reaktivno apdejtovanje
+        walletAccountRepo.observeActiveWalletId(userId: pubkey)
+            .toPublisher()
+            .sink { walletId in
+                print("WALLET ID \(walletId ?? "nil")")
+                
+            }
+            .store(in: &cancellables)
+        
+        Task {
+            let res = try await ConnectNwcUseCase(walletRepository: walletRepo, walletAccountRepository: walletAccountRepo).invoke(userId: pubkey, nwcUrl: url).exceptionOrNull()
+            
+            print("WALLET SUCCES \(res)")
+            
+            guard let walletID = try await walletAccountRepo.getActiveWallet(userId: pubkey)?.walletId else { return }
+            
+            let balance = try await walletRepo.fetchWalletBalance(walletId: walletID).getOrThrow()
+            
+            print(balance)
+            
+            // Za receive ekran
+//            try await walletRepo.createLightningInvoice(walletId: walletID, amountInBtc: nil, comment: nil)
+//            
+//            
+//            try await walletRepo.pay(walletId: walletID, request: .LightningLnInvoice(amountSats: "10", noteRecipient: "message override", noteSelf: "message override", lnInvoice: "INVOICE STRING"))
+//            
+//            try await walletRepo.pay(walletId: walletID, request: .LightningLnUrl(amountSats: "", noteRecipient: "", noteSelf: "", lnUrl: "", lud16: ""))
+            
+        }
     }
 }
 
@@ -131,13 +166,13 @@ extension NWCWalletManager: WalletImplementation {
     }
     
     func sendLNInvoice(_ lninvoice: String, satsOverride: Int?, messageOverride: String?) async throws {
-        var amount: KotlinLong? = nil
-        if let satsOverride {
-            amount = KotlinLong(value: Int64(satsOverride))
-        }
-        let res = try await client.payInvoice(params: .init(invoice: lninvoice, amount: amount))
+//        var amount: KotlinLong? = nil
+//        if let satsOverride {
+//            amount = KotlinLong(value: Int64(satsOverride))
+//        }
+//        let res = try await client.payInvoice(params: .init(invoice: lninvoice, amount: amount))
         
-        print(res.description)
+//        print(res.description)
     }
     
     func sendLNURL(lnurl: String, pubkey: String?, sats: Int, note: String, zap: NostrObject?) async throws {
@@ -188,7 +223,17 @@ extension NWCWalletManager: WalletImplementation {
 
 extension NWCWalletManager: NostrEventSignatureHandler {
     func __signNostrEvent(unsignedNostrEvent: NostrUnsignedEvent, completionHandler: @escaping @Sendable (SignResult?, (any Error)?) -> Void) {
-        SignResult.Signed(event: .init(id: <#T##String#>, pubKey: <#T##String#>, createdAt: <#T##Int64#>, kind: <#T##Int32#>, tags: NostrExtensions.shared.mapAsListOfJsonArray(tags: <#T##[[String]]#>), content: <#T##String#>, sig: <#T##String#>))
+        let tags = NostrExtensions.shared.mapAsListOfListOfStrings(tags: unsignedNostrEvent.tags)
+        guard let object = NostrObject.create(content: unsignedNostrEvent.pubKey, kind: Int(unsignedNostrEvent.kind), tags: tags) else {
+            completionHandler(SignResult.Rejected(error: .init(message: "Failed to sign", cause: nil)), nil)
+            return
+        }
+        
+        completionHandler(
+            SignResult.Signed(event: .init(
+                id: object.id, pubKey: object.pubkey, createdAt: object.created_at, kind: Int32(object.kind), tags: NostrExtensions.shared.mapAsListOfJsonArray(tags: object.tags), content: object.content, sig: object.sig
+            )), nil
+        )
     }
     
     
