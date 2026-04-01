2527e1009e3cfcc5519dbbf395cd36c9daab6968
Pavle Stevanović <pavlekzn@gmail.com>
2026-04-01 16:03:23 +0200

    Update feed gallery user cell.

--- Primal/Scenes/Feed/FeedElementCells/Gallery/GalleryFeedElementUserCell.swift
+++ Primal/Scenes/Feed/FeedElementCells/Gallery/GalleryFeedElementUserCell.swift
@@ -14,21 +14,13 @@ class GalleryFeedElementUserCell: UITableViewCell, RegularFeedElementCell {
     weak var delegate: FeedElementCellDelegate?
 
     let threeDotsButton = UIButton()
-    let profileImageView = UserImageView(height: FontSizeSelection.current.avatarSize)
+    let profileImageView = UserImageView(height: 32)
     let checkbox = VerifiedView().constrainToAspect(1, priority: .required)
     let nameLabel = UILabel()
-    let timeLabel = UILabel()
     let nipLabel = UILabel()
-    let repostIndicator = RepostedIndicatorView()
-    let separatorLabel = UILabel()
-    let replyingToView = ReplyingToView()
-    lazy var nameStack = UIStackView([nameLabel, checkbox, nipLabel, separatorLabel, timeLabel])
-    lazy var nameReplyStack = UIStackView(axis: .vertical, [nameStack, replyingToView])
-    lazy var nameSuperStack = UIStackView([profileImageView, nameReplyStack])
-    lazy var mainStack = UIStackView(axis: .vertical, [repostIndicator, nameSuperStack])
-    let threeDotsSpacer = SpacerView(width: 20, priority: .required)
-
-    let repostedByOverlayButton = UIButton()
+    lazy var nameRow = UIStackView([nameLabel, checkbox, UIView()])
+    lazy var labelStack = UIStackView(axis: .vertical, [nameRow, nipLabel])
+    lazy var mainStack = UIStackView([profileImageView, labelStack, threeDotsButton])
 
     var checkboxHeightC: NSLayoutConstraint?
 
@@ -39,60 +31,33 @@ class GalleryFeedElementUserCell: UITableViewCell, RegularFeedElementCell {
         backgroundColor = .clear
 
         contentView.addSubview(mainStack)
-        mainStack.pinToSuperview(edges: .top, padding: 12).pinToSuperview(edges: .leading, padding: 12).pinToSuperview(edges: .trailing, padding: 16)
+        mainStack.pinToSuperview(edges: .top, padding: 12).pinToSuperview(edges: .leading, padding: 12).pinToSuperview(edges: .trailing, padding: 12)
         let botC = mainStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -6)
         botC.priority = .defaultLow
         botC.isActive = true
 
-        mainStack.spacing = 4
-        nameReplyStack.spacing = 4
-        nameSuperStack.spacing = 8
+        mainStack.alignment = .center
+        mainStack.spacing = 10
 
-        contentView.addSubview(threeDotsButton)
-        threeDotsButton
-            .constrainToSize(44)
-            .pinToSuperview(edges: .top, padding: 2)
-            .pinToSuperview(edges: .trailing)
+        labelStack.spacing = 2
 
-        nameStack.addArrangedSubview(threeDotsSpacer)
-
-        contentView.addSubview(repostedByOverlayButton)
-        repostedByOverlayButton
-            .constrainToSize(width: 100)
-            .pin(to: repostIndicator, edges: .leading)
-            .pin(to: repostIndicator, edges: .top, padding: -11)
-            .pin(to: repostIndicator, edges: .bottom, padding: -5)
-
-        repostedByOverlayButton.addAction(.init(handler: { [weak self] _ in
-            guard let self else { return }
-            delegate?.postCellDidTap(self, .repostedProfile)
-        }), for: .touchUpInside)
-
-        separatorLabel.text = "·"
-
-        [nameLabel, nipLabel, separatorLabel].forEach { $0.setContentHuggingPriority(.required, for: .horizontal) }
-
-        nipLabel.lineBreakMode = .byTruncatingTail
-        separatorLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
-        timeLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
-        nameLabel.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
-        nipLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
-
-        threeDotsButton.setContentHuggingPriority(.required, for: .horizontal)
+        nameRow.alignment = .center
+        nameRow.spacing = 4
 
         checkboxHeightC = checkbox.heightAnchor.constraint(equalToConstant: FontSizeSelection.current.contentFontSize)
         checkboxHeightC?.isActive = true
-        nameStack.alignment = .center
-        nameStack.spacing = 4
 
+        threeDotsButton.constrainToSize(44)
         threeDotsButton.setImage(UIImage(named: "threeDots"), for: .normal)
         threeDotsButton.showsMenuAsPrimaryAction = true
+
+        nipLabel.lineBreakMode = .byTruncatingTail
 
         let tapArea = UIView()
         contentView.addSubview(tapArea)
         tapArea
             .pin(to: profileImageView, edges: [.leading, .vertical])
-            .pin(to: separatorLabel, edges: .trailing)
+            .pin(to: labelStack, edges: .trailing)
         tapArea.addGestureRecognizer(BindableTapGestureRecognizer(action: { [unowned self] in
             delegate?.postCellDidTap(self, .profile)
         }))
@@ -114,34 +79,11 @@ class GalleryFeedElementUserCell: UITableViewCell, RegularFeedElementCell {
             checkbox.isHidden = true
         }
 
-        let date = Date(timeIntervalSince1970: TimeInterval(parsedContent.post.created_at))
-        timeLabel.text = date.timeAgoDisplay()
-
         profileImageView.setUserImage(parsedContent.user)
-
-        if let parent = parsedContent.replyingTo {
-            replyingToView.userNameLabel.text = parent.user.data.firstIdentifier
-            replyingToView.isHidden = false
-            nameSuperStack.alignment = .top
-        } else {
-            replyingToView.isHidden = true
-            nameSuperStack.alignment = .center
-        }
-
-        threeDotsSpacer.isHidden = parsedContent.reposted != nil
-        threeDotsButton.transform = parsedContent.reposted != nil ? .init(translationX: 0, y: -6) : (parsedContent.replyingTo == nil ? .init(translationX: 0, y: 4) : .identity)
-
-        if let reposted = parsedContent.reposted?.users {
-            repostIndicator.update(users: reposted)
-            repostIndicator.isHidden = false
-        } else {
-            repostIndicator.isHidden = true
-        }
-        repostedByOverlayButton.isHidden = parsedContent.reposted == nil
 
         threeDotsButton.menu = .init(children: parsedContent.actionsData().map { (title, image, action, attributes) in
             UIAction(title: title, image: image, attributes: attributes) { [weak self] _ in
-                guard let self = self else { return }
+                guard let self else { return }
                 delegate?.postCellDidTap(self, action)
             }
         })
@@ -159,9 +101,7 @@ class GalleryFeedElementUserCell: UITableViewCell, RegularFeedElementCell {
 
         checkboxHeightC?.constant = FontSizeSelection.current.contentFontSize
 
-        [timeLabel, separatorLabel, nipLabel].forEach {
-            $0.font = .appFont(withSize: FontSizeSelection.current.nameSize, weight: .regular)
-            $0.textColor = .foreground3
-        }
+        nipLabel.font = .appFont(withSize: FontSizeSelection.current.nameSize, weight: .regular)
+        nipLabel.textColor = .foreground3
     }
 }
