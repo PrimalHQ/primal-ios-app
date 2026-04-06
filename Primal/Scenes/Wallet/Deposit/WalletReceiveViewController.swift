879478616ba32f2f741accacbdb2f70101d239be
Pavle Stevanović <pavlekzn@gmail.com>
2026-04-06 21:47:37 +0200

    Fix receive wallet success screen.

--- Primal/Scenes/Wallet/Deposit/WalletReceiveViewController.swift
+++ Primal/Scenes/Wallet/Deposit/WalletReceiveViewController.swift
@@ -132,7 +132,7 @@ private extension WalletReceiveViewController {
         monitorInvoice = invoice
         monitorTask?.cancel()
         monitorTask = Task { @MainActor [weak self] in
-            let result = try await wallet.walletRepo.awaitLightningPayment(walletId: walletID, invoice: invoice, timeout: 300)
+            let result = try await wallet.walletRepo.awaitLightningPayment(walletId: walletID, invoice: invoice, timeout: .max)
             
             if result.getOrNull() != nil, let self, self.navigationController?.topViewController == self {
                 navigationController?.pushViewController(WalletTransferSummaryController(.success(title: text, description: [])), animated: true)
