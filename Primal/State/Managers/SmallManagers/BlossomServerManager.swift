ecce6cf472936e418f51ee0df24c00258e644c82
Pavle Stevanović <pavlekzn@gmail.com>
2026-04-06 12:40:41 +0200

    Fix blossom server on share extension

--- Primal/State/Managers/SmallManagers/BlossomServerManager.swift
+++ Primal/State/Managers/SmallManagers/BlossomServerManager.swift
@@ -10,18 +10,27 @@ import GenericJSON
 
 class BlossomServerManager {
     static let instance = BlossomServerManager()
-    
+
+    private let defaults = UserDefaults.standard
+    private let defaultsKey = "blossomServerInfo"
+
     var info: [String: [String]] = [:]
-    
+
+    init() {
+        if let persisted = defaults.dictionary(forKey: defaultsKey) as? [String: [String]] {
+            info = persisted
+        }
+    }
+
     @MainActor
     func addBlossomInfo(_ infos: [String: JSON]) {
         guard
             let pubkey = infos["pubkey"]?.stringValue,
             let tags: [[JSON]] = infos["tags"]?.arrayValue?.compactMap({ $0.arrayValue })
         else { return }
-        
+
         var serverURLs: [String] = []
-        
+
         for tag in tags {
             guard
                 tag.first?.stringValue == "server",
@@ -29,10 +38,11 @@ class BlossomServerManager {
             else { continue }
             serverURLs.append(url)
         }
-        
+
         if serverURLs.isEmpty { return }
         info[pubkey] = serverURLs
+        defaults.set(info, forKey: defaultsKey)
     }
-    
+
     func serversForUser(pubkey: String) -> [String]? { info[pubkey] }
 }
