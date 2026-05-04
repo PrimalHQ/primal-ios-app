0b88c399c1f45583ff9bcc710b9cc538c6c3e79c
Pavle Stevanović <pavlekzn@gmail.com>
2026-05-04 12:56:20 +0200

    Fix wallet issues.

--- Primal/Scenes/RootViewController/MainTabBar/WalletDetectedPopupController.swift
+++ Primal/Scenes/RootViewController/MainTabBar/WalletDetectedPopupController.swift
@@ -53,7 +53,7 @@ class WalletDetectedPopupController: UIViewController {
 
         let titleText = isDiscontinued ? "Wallet Discontinued" : "Wallet Detected"
         let descriptionText = isDiscontinued
-            ? "Your custodial Primal wallet has been discontinued. To continue using the Primal wallet, please restore your non-custodial wallet via the recovery phrase, or create a new wallet."
+            ? "The custodial wallet service has been discontinued. To recover your funds, please contact us at support@gmail.net."
             : "We detected that you already have a non-custodial Primal wallet associated with this Nostr account. To use it on this device, please restore it via the recovery phrase. Alternatively, you can create a new wallet which will be associated with your account."
 
         let descLabel = UILabel()
