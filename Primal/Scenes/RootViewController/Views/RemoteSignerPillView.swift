cf87c3e5614a33160bc65a9351976511a3d7a305
Pavle Stevanović <pavlekzn@gmail.com>
2026-05-13 12:17:21 +0200

    add glass

--- Primal/Scenes/RootViewController/Views/RemoteSignerPillView.swift
+++ Primal/Scenes/RootViewController/Views/RemoteSignerPillView.swift
@@ -77,6 +77,15 @@ class RemoteSignerPillView: UIView, Themeable {
         messageView.centerToSuperview()
         iconView.pinToSuperview(edges: [.trailing, .top])
         
+        if #available(iOS 26.0, *) {
+            let effect = UIVisualEffectView(effect: UIGlassEffect(style: .regular))
+            effect.cornerConfiguration = .capsule()
+            iconView.insertSubview(effect, at: 0)
+            effect.pinToSuperview()
+        } else {
+            // Fallback on earlier versions
+        }
+        
         layer.zPosition = 900
         isHidden = true
         
@@ -88,7 +97,11 @@ class RemoteSignerPillView: UIView, Themeable {
     func updateTheme() {
         messageView.backgroundColor = .foreground
         messageLabel.textColor = .background
-        iconView.backgroundColor = .background
+        if #available(iOS 26.0, *) {
+            iconView.backgroundColor = .clear
+        } else {
+            iconView.backgroundColor = .background
+        }
         
         icon.tintColor = .foreground
         alternateIcon.tintColor = .background3
