4c4bbd74c4289334e16a35ebb58c4aebdda9e91f
Pavle Stevanović <pavlekzn@gmail.com>
2026-06-25 15:53:55 +0200

    Fix reply toast.

--- Primal/State/Managers/PostingManager.swift
+++ Primal/State/Managers/PostingManager.swift
@@ -100,7 +100,7 @@ final class PostingManager {
             let isRoot = !obj.tags.contains(where: { tag in tag[safe: 3] == "root" })
             
             DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)) {
-                RootViewController.instance.finalChild.view.showToast("\(isRoot ? "Note" : "Reply") Published")
+                RootViewController.instance.showToast("\(isRoot ? "Note" : "Reply") Published")
             }
         }
         .store(in: &cancellables)
